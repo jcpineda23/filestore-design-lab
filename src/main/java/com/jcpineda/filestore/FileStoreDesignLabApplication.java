55a4a0a74fb01b5f6ddf2a283a1e8aae08240309
Juan Pineda <jcpineda@outlook.com>
2026-02-28 12:51:50 -0800

    feat: bootstrap spring app and add initial db migrations

---
+++ src/main/java/com/jcpineda/filestore/FileStoreDesignLabApplication.java
@@ -0,0 +1,12 @@
+package com.jcpineda.filestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStoreDesignLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStoreDesignLabApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jcpineda/filestore/FileStoreDesignLabApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jcpineda.filestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FileStoreDesignLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
